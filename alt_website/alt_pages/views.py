9312998ffea8b7c20e616a3ce45c50e4a34f3359
Joey Wilhelm <joey@scottsmarketplace.com>
2013-06-01 17:43:46 -0700

    * Getting the header & footer showing globally * Got the mailchimp subscription to return to the previous page * Update the mailchimp subscribe message

--- alt_website/alt_pages/views.py
+++ alt_website/alt_pages/views.py
@@ -10,6 +10,7 @@ from alt_pages.forms import MailChimpForm
 @require_POST
 def list_subscribe(request):
     form = MailChimpForm(data=request.POST)
+    next_page = request.POST.get('next', '/')
     if form.is_valid():
         chimp_list = utils.get_connection().get_list_by_id(
                                                    settings.MAILCHIMP_LIST_ID)
@@ -25,13 +26,14 @@ def list_subscribe(request):
         if request.is_ajax():
             return HttpResponse(status=200)
         else:
-            messages.success(request, 'You have been subscribe to the mailing '
-                             'list.')
-            return HttpResponseRedirect('/')
+            messages.success(request, 'You have been sent a confirmation email. '
+                             'Please follow the instructions within to confirm '
+                             'your subscription.')
+            return HttpResponseRedirect(next_page)
     else:
         if request.is_ajax():
             return HttpResponse(status=400)
         else:
             messages.error(request, 'Please fill in your name and a valid '
                            'email address.')
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect(next_page)
